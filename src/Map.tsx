702a39f0b8134b903a7bf8255c9b963017fa3893
User <ignalelop@alum.us.es>
2017-10-16 13:02:51 +0200

    Comentada las clases Cell, index y Map

--- src/Map.tsx
+++ src/Map.tsx
@@ -6,13 +6,17 @@ import { Actions, State, InitialState, Reducer } from './GameState';
 import { Cell } from './Cell';
 import { Unit } from './Unit';
 
+/** Representa el mapa que contendrá las unidades y las casillas **/
 export class Map extends React.Component<any, any> {
+    /** @constructor  Deben introducirse los elementos horizontal y vertical **/
     constructor(props: any) {
         super(props);
         store.dispatch(Actions.generateSetListener(this));
     }
 
+    /** Renderiza el mapa **/
     render() {
+        // El mapa se renderizará en un div con estilo, por ello debemos usar className="map"
         return (
             <div id="map" className="map" onClick={this.onClick.bind(this)}>
                 {this.generateMap().map((a: any) => {
@@ -22,22 +26,28 @@ export class Map extends React.Component<any, any> {
         );
     }
 
+    /** Placeholder, contendrá la lógica de selección de la casilla correcta. **/
     onClick(e : React.MouseEvent<HTMLElement>) {
         console.log("Clicked on: ("+e.clientX+", "+e.clientY+")");
 
     }
 
+    /** Función auxiliar usada para renderizar el mapa. Consiste en recorrer todas las columnas acumulando las casillas. **/
     generateMap() {
         var accum = [];
+        // Repetirá este for hasta que se llegue al número de columnas especificado
         for(var i = 0; i < this.props.vertical; i++) {
+            // Este método retornará una lista con las casillas en fila
             accum.push(this.generateCellRow(i));
         }
 
         return accum;
     }
 
+    /** Función auxiliar que servirá para generar las casillas en una fila **/
     generateCellRow(num_row: number) {
         var accum2 = [];
+        // Este bucle iterará hasta el número de celdas horizontales especificado en el props.
         for(var j = 0; j < this.props.horizontal; j++) {
             if(j == store.getState().position.x && num_row == store.getState().position.y){
                 accum2.push(
@@ -46,12 +56,14 @@ export class Map extends React.Component<any, any> {
                     </div>
                 );
             }else{
+                // Se introducirá el elemento en una lista
                 accum2.push(
                     <Cell horizontal={j} vertical={num_row} />
                 );
             }
         }
 
+        // Se retorna en un div que dependiendo de que se trate de la fila par o impar, contendrá también la clase celRowOdd.
         return (
             <div className={"cellRow" + (num_row%2==0?"":" cellRowOdd")}>
                 {accum2}
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Map } from './Map';
 
+// Representa la aplicación, por ahora únicamente el mapa
 ReactDOM.render(<Map horizontal="9" vertical="12" />, document.getElementById("root"));
